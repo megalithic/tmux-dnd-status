887332e4fa9c8bd8b27d12bc659e21b3d4727781
Seth Messer <seth.messer@gmail.com>
2021-06-23 14:19:22 -0500

    fix logic --boo

--- scripts/dnd_status.sh
+++ scripts/dnd_status.sh
@@ -34,10 +34,8 @@ dnd_off_icon_default() {
 
 dnd_status() {
   if is_osx; then
-    # status=$(defaults -currentHost read ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb)
     status=$(calm-notifications status)
-
-    $([[ "$status" -eq "on" ]] && true || false)
+    $([[ "$status" == "on" ]] && true || false)
   fi
 }
 
